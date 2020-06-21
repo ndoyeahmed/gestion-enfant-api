8a9e363d015a7d11faa560ae7929f2d751e7fc89
Mouhamed NDOYE <ndoyeahmed2602@gmail.com>
2020-06-21 06:34:10 +0000

    deploiement en ligne

--- src/main/java/com/gestion/stock/StockApiApplication.java
+++ src/main/java/com/gestion/stock/StockApiApplication.java
@@ -1,6 +1,5 @@
 package com.gestion.stock;
 
-import com.gestion.stock.config.SpringSecurityAuditorAware;
 import com.gestion.stock.services.admin.UtilisateurService;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +7,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -27,10 +24,10 @@ public class StockApiApplication extends SpringBootServletInitializer implements
     private UtilisateurService utilisateurService;
 
 
-    @Bean
-    public AuditorAware<String> auditorAware() {
-        return new SpringSecurityAuditorAware();
-    }
+//    @Bean
+//    public AuditorAware<String> auditorAware() {
+//        return new SpringSecurityAuditorAware();
+//    }
 
     @Autowired
     public void setBCryptPasswordEncoder(BCryptPasswordEncoder bCryptPasswordEncoder) {
